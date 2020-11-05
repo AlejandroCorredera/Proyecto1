c9bc47837beed759e370fc81b3e5768456ab86c9
ASUS <ASUS@DESKTOP-MEQ9N8J>
2020-11-05 18:13:02 +0100

    Cambios. Fase 2.

--- Proyecto 1/src/Prueba1/principal.java
+++ Proyecto 1/src/Prueba1/principal.java
@@ -4,7 +4,10 @@ public class principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+			System.out.println("Hola mundo java-git");
+			System.out.println("Hola mundo java-git");
+			System.out.println("Hola mundo java-git");
 	}
 
 }
